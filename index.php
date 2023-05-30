4326821a61dcc42d856fc69ea2fbba7d7d3a854e
Nathalia Molina Da Silva <112526050+Nathy019@users.noreply.github.com>
2023-05-30 11:05:19 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>Daanihel</title>
+</head>
+
+<body>
+  <!-- começo do cabeçalho-->
+  <header class="menu-principal">
+    <nav class="menu">
+      <div style="background: #ffff;" id="barra">
+      <ul>
+        <li><a href="quemsoueu.html" class="ativo">Quem sou eu</a></li>
+        <li><a href="portfolio.html" class="ativo">Portfólio</a></li>
+        <li><a href="materiais.html" class="ativo">Materiais</a></li>
+        <li><a href="bioseguranca.html" class="ativo">Biosegurança</a></li>
+        <li><a href="contato.html" class="ativo" id="contato">Contato</a></li>
+      </ul>
+    </div>
+      <div class="logo">
+        <a href="index.html"><img src="/imagens/logo.png"></a>
+
+      </div>
+      <div>
+        <h2>DAANIHEL</h2>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3679.1138447947537!2d-47.41020868523575!3d-22.761155985086063!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c89c3364ae418d%3A0x53e33c811ebf1229!2sR.%20Argentina%2C%20378%20-%20Jardim%20Belo%20Horizonte%2C%20Santa%20B%C3%A1rbara%20d&#39;Oeste%20-%20SP%2C%2013450-181!5e0!3m2!1spt-BR!2sbr!4v1674491439539!5m2!1spt-BR!2sbr"
+          width="280" height="280" style="border: 10px;" allowfullscreen="" loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </div>
+      <div class="redes-sociais">
+        <ul>
+          <li>
+            <a href="https://api.whatsapp.com/send?phone=5519994412860&fbclid=PAAaZhRJT6BwdZ5bT99kfirR_0fhjNH9xlZMsoIGKzUdw3mrQseFQLCteY6Jk"
+              target="_blank">
+              <img src="/imagens/zap.png" width="50px" alt="logo whatsapp">
+            </a>
+          </li>
+
+          <li>
+            <a href="https://www.instagram.com/daanihell.ttt/" target="_blank">
+              <img src="/imagens/insta.png" width="50px" alt="logo instagram">
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="pagamentos">
+        <ul>
+          <li>
+            <img src="/imagens/cartao.png" alt="logo cartao">
+            <p>Aceitamos débito e crédito</p>
+            <img class="pixx" src="/imagens/pix.png" alt="logo pix">
+            <p>Pix e dinheiro</p>
+          </li>
+        </ul>
+      </div>
+      <div class="marcas">
+        <ul>
+          <li>>
+            <img src="/imagens/marca-cartao.png" width="180px" alt="marcas cartao">
+            <p>Pagamentos a vista com desconto</p>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+  <!-- Fim do cabeçalho-->
+
+</body>
+
+</html>
+
+<!--
+  1- Definição do escopo de seu projeto?
+   R: houve a finalização da primeira página do site, ja as outras ainda estão em andamento
+
+  2- Como está o andamento do seu projeeto?
+  R: finalizamos a primeira parte e estamos começando a editar a segunda parte faltando pouco para sua finalização, pretendemos dar inicio na terceira em breve
+
+  3- Definição das "atividades" como é que estão?
+  R: Teve inicio durante as aulas demos continuedade em casa e durante as aulas fazemos alguns ajustes
+
+ 4- Prazo e cronograma das atividades?
+ R: Não temos um prazo definido mas está tudo correndo como planejado
+-->
